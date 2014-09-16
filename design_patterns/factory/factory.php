22f2d22c5a2fb4eb3e3b084e9153af471cb0be92
eandbteam <pascalfelger@gmail.com>
2014-09-16 19:23:29 +0200

    2014-09-16 refactor factory class with sample ==>http://www.phptherightway.com/pages/Design-Patterns.html

---
+++ design_patterns/factory/factory.php
@@ -0,0 +1,8 @@
+<?php
+include 'factory_class.php';
+
+
+// have the factory create the Automobile object
+$veyron = AutomobileFactory::create('Bugatti', 'Veyron');
+
+print_r($veyron->get_make_and_model()); // outputs "Bugatti Veyron"
