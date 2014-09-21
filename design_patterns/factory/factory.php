1c9e826c7ec293ea164ea5157b80ab2b6e7dc125
eandbteam <pascalfelger@gmail.com>
2014-09-21 18:04:25 +0200

    2014-09-21 maj

--- design_patterns/factory/factory.php
+++ design_patterns/factory/factory.php
@@ -25,6 +25,8 @@ catch (RuntimeException $e)
 {
   echo $e->getMessage();
 }
+
+/////////////////////////////MySQL////////////////////////////////////////////////////////////
 $instance = MySQL::getInstance($host='localhost',$user='root',$password='',$database='test');
 $query = $instance->sendQuery("select * from polo");
 
@@ -40,7 +42,7 @@ $instance->Close();
 $instance2 = MySQL::getInstance($host='192.168.1.35',$user='test',$password='test',$database='test');
 
 var_dump($instance2);
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
 try
 {
@@ -53,7 +55,7 @@ catch (RuntimeException $e)
 }
 */
 
-
+/////////////////////////////////PHPBookPrototype//////////////////////////////////////////////////////
 try
 {
 	$prototype = DBFactory::load($config_prototype);
@@ -64,9 +66,36 @@ catch (RuntimeException $e)
 }
 
 
+writeln('BEGIN TESTING PROTOTYPE PATTERN');
+writeln('');
 
+$phpProto = new PHPBookPrototype();
+$sqlProto = new SQLBookPrototype();
 
+$book1 = clone $sqlProto;
+$book1->setTitle('SQL For Cats');
+writeln('Book 1 topic: '.$book1->getTopic());
+writeln('Book 1 title: '.$book1->getTitle());
+writeln('');
 
+$book2 = clone $phpProto;
+$book2->setTitle('OReilly Learning PHP 5');
+writeln('Book 2 topic: '.$book2->getTopic());
+writeln('Book 2 title: '.$book2->getTitle());
+writeln('');
+
+$book3 = clone $sqlProto;
+$book3->setTitle('OReilly Learning SQL');
+writeln('Book 3 topic: '.$book3->getTopic());
+writeln('Book 3 title: '.$book3->getTitle());
+writeln('');
+
+writeln('END TESTING PROTOTYPE PATTERN');
+
+function writeln($line_in) {
+	echo $line_in."<br/>";
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
